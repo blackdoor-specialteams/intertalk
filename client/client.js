8080ba8d1c0bf347cb0ca5d3ee2e3615cceb64c8
yacklebeam <jaketroxel@gmail.com>
2016-09-12 21:36:42 -0700

    Changed type of AJAX to text/plain

--- client/client.js
+++ client/client.js
@@ -124,8 +124,7 @@ function initPage()
                 url: "https://" + connectURL + ":4567/users/",
                 type: "POST",
                 data:package,
-                contentType:"application/json; charset=utf-8",
-                dataType:'json'
+                contentType:"text/plain; charset=utf-8",
             });
         }
         else
