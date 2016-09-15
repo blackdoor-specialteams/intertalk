78425aef8fd5b93f83b3f32b52d939b1a6ec8466
tclostio <twclostio@protonmail.com>
2016-09-15 12:15:34 -0700

    switched email validation methods

--- src/main/java/black/door/intertalk/MailAddress.java
+++ src/main/java/black/door/intertalk/MailAddress.java
@@ -7,12 +7,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.apache.commons.validator.routines.EmailValidator;
 import javaslang.control.Try;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.io.IOException;
-
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -41,10 +41,12 @@ public class MailAddress implements Comparable{
 
 	public static boolean Validate(String addr) {
 		boolean result = true;
+        boolean local = true;
 		try {
 			InternetAddress email = new InternetAddress(addr);
 			email.validate();
 
+            EmailValidator.getInstance(local).isValid(addr);
 			// if (email.toString().contains("localhost")) result = false;
 			// if (email.toString().contains("127.0.0.1")) result = false;
 
