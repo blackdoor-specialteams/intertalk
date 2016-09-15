b373bb828f45059976e5408f6b6bf7bceb6a75af
Trent Clostio <twclostio@protonmail.com>
2016-09-15 10:28:16 -0700

    validation that maybe works

--- src/main/java/black/door/intertalk/MailAddress.java
+++ src/main/java/black/door/intertalk/MailAddress.java
@@ -27,13 +27,32 @@ public class MailAddress implements Comparable{
 	public MailAddress(String local, String domain) throws AddressException {
 		this.local = requireNonNull(local).toLowerCase();
 		this.domain = requireNonNull(domain).toLowerCase();
-		new InternetAddress(local + '@' + domain, true);
+		String addr = local + '@' + domain;
+		if(Validate(addr)) {
+			new InternetAddress(local + '@' + domain, true);
+		}
 	}
 
 	public static Try<MailAddress> parse(String addr){
 		requireNonNull(addr);
 		int index = addr.lastIndexOf('@');
 		return Try.of(() -> new MailAddress(addr.substring(0, index), addr.substring(index+1, addr.length())));
+	}
+
+	public static boolean Validate(String addr) {
+		boolean result = true;
+		try {
+			InternetAddress email = new InternetAddress(addr);
+			email.validate();
+
+			// if (email.toString().contains("localhost")) result = false;
+			// if (email.toString().contains("127.0.0.1")) result = false;
+
+		} catch (AddressException ex) {
+			result = false;
+		}
+
+		return result;
 	}
 
 	@Override
